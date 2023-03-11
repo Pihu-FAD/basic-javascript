4f291354eb46bfe12ec45f16e1ad2997201f8ee3
Pihu <pushpachaudhary027@gmail.com>
2023-03-11 14:36:32 +0530

    Airthmatic Operator

---
+++ Airthmatic Operator/task.js
@@ -0,0 +1,42 @@
+// const radius=5;
+// const pi=Math.PI;
+// const area= pi*radius*radius;
+// // console.log(`area of circle is${area}`);
+// console.log('area of circle', area);
+
+
+
+
+// const height=8;
+// const base=5;
+// const AofT = 0.5 * base* height;
+// console.log(`Area of Triangle is ${AofT}`);
+
+
+// const base1=15;
+// const base2=20;
+// const height=5;
+// const AofTrap= 0.5*(base1+base2)*height;
+// console.log(`Area of Trapezoid is ${AofTrap}`);
+
+
+// const radius=8;
+// const pi=Math.PI;
+// const PofC = 2* pi*radius;
+// console.log(`Perimeter of Cirele is : ${PofC}`);
+
+
+
+// const a =2;
+// const b =5;
+// const c =8;
+// const PofTri = a+b+c;
+// console.log(`Perimeter of Triangle is : ${PofTri}`);
+
+
+// const a=2;
+// const b=2;
+// const c=2;
+// const d=2;
+// const PofTrap= a+b+c+d;
+// console.log(`Perimeter of Trapezoid is : ${PofTrap}`);
